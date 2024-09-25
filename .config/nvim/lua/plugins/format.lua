ed8ff720d6e646878e7ba4036002f326bb8fadf2
omeyenburg <oskar.meyenburg@gmail.com>
2024-09-25 18:42:47 +0200

    Nvim: disable formatting on save

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -50,13 +50,13 @@ return {
         },
         opts = {
             notify_on_error = true,
-            format_on_save = function(bufnr)
-                local disable_filetypes = { c = true, cpp = true }
-                return {
-                    timeout_ms = 500,
-                    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-                }
-            end,
+            -- format_on_save = function(bufnr)
+            --     local disable_filetypes = { c = true, cpp = true }
+            --     return {
+            --         timeout_ms = 500,
+            --         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+            --     }
+            -- end,
             formatters_by_ft = {
                 bash = { 'shfmt' }, -- Bash formatter
                 c = { 'clang-format' }, -- C formatter
