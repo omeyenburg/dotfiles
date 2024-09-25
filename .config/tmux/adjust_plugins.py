acd6d962814fa7a4aeffa2f8e9cd83932bdbbc90
omeyenburg <oskar.meyenburg@gmail.com>
2024-09-25 21:31:33 +0200

    Tmux: tokyonight status bar colors

--- .config/tmux/adjust_plugins.py
+++ .config/tmux/adjust_plugins.py
@@ -1,6 +1,21 @@
-line = '  tmux set-option -g pane-border-style "fg=${gray}"'
-with open('.config/tmux/plugins/tmux-dracula/scripts/dracula.sh', 'r+') as f:
+lines = [
+    ['  tmux set-option -g pane-border-style "fg=${gray}"', ""],
+    [
+        '"#[fg=${gray},bg=${dark_purple}]${left_sep}#[fg=${white},bg=${dark_purple}] #I #W${current_flags} #[fg=${dark_purple},bg=${gray}]${left_sep}"',
+        '"#[fg=${gray},bg=${dark_purple}]${left_sep}#[fg=${gray},bg=${dark_purple}] #I #W${current_flags} #[fg=${dark_purple},bg=${gray}]${left_sep}"',
+    ],
+    [
+        '"#[fg=${white},bg=${dark_purple}] #I #W${current_flags} "',
+        '"#[fg=${gray},bg=${dark_purple}] #I #W${current_flags} "',
+    ],
+]
+
+with open(".config/tmux/plugins/tmux-dracula/scripts/dracula.sh", "r+") as f:
     content = f.read()
+
+    for line in lines:
+        content = content.replace(line[0], line[1])
+
     f.seek(0)
-    f.write(content.replace(line, ''))
+    f.write(content)
     f.truncate()
