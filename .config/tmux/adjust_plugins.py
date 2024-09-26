9fdb5af582838f91b90d248f2375fa07d920d56e
omeyenburg <oskar.meyenburg@gmail.com>
2024-09-26 23:41:53 +0200

    Tmux: Add python3 fallback

--- .config/tmux/adjust_plugins.py
+++ .config/tmux/adjust_plugins.py
@@ -1,21 +1,29 @@
-lines = [
-    ['  tmux set-option -g pane-border-style "fg=${gray}"', ""],
-    [
+lines = (
+    (
+        '  tmux set-option -g pane-border-style "fg=${gray}"',
+        "",
+    ),
+    (
         '"#[fg=${gray},bg=${dark_purple}]${left_sep}#[fg=${white},bg=${dark_purple}] #I #W${current_flags} #[fg=${dark_purple},bg=${gray}]${left_sep}"',
         '"#[fg=${gray},bg=${dark_purple}]${left_sep}#[fg=${gray},bg=${dark_purple}] #I #W${current_flags} #[fg=${dark_purple},bg=${gray}]${left_sep}"',
-    ],
-    [
+    ),
+    (
         '"#[fg=${white},bg=${dark_purple}] #I #W${current_flags} "',
         '"#[fg=${gray},bg=${dark_purple}] #I #W${current_flags} "',
-    ],
-]
+    ),
+)
 
-with open(".config/tmux/plugins/tmux-dracula/scripts/dracula.sh", "r+") as f:
-    content = f.read()
+try:
+    with open(".config/tmux/plugins/tmux-dracula/scripts/dracula.sh", "r+") as f:
+        content = f.read()
 
-    for line in lines:
-        content = content.replace(line[0], line[1])
+        for line in lines:
+            content = content.replace(line[0], line[1])
 
-    f.seek(0)
-    f.write(content)
-    f.truncate()
+        f.seek(0)
+        f.write(content)
+        f.truncate()
+except FileNotFoundError:
+    print(
+        "Could not find dracula tmux plugin. Perhaps it is not installed in .config/tmux/plugins?"
+    )
