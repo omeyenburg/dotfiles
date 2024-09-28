69fc3e4cea798a907670108d045751af70467650
omeyenburg <oskar.meyenburg@gmail.com>
2024-09-28 08:36:30 +0200

    Gitlog command english output

--- .config/shell/gitlog.py
+++ .config/shell/gitlog.py
@@ -1,5 +1,6 @@
-import sys
 import subprocess
+import sys
+import os
 
 length = 5
 format = "%s\n%an\n%as\n(%ar)"
@@ -16,27 +17,36 @@ command = [
 ]
 
 try:
+    # Set git output language
+    os.environ["LANG"] = "en_GB"
+
+    # Run git log command and capture output
     result = subprocess.run(command, capture_output=True, text=True, check=True)
     lines = list(map(str.strip, result.stdout.replace("\n\n", "\n").splitlines()))
 
+    # Extract commits
     num_values = format.count("\n") + 2
     length = len(lines) // num_values
-
     commits = [lines[i * num_values : (i + 1) * num_values] for i in range(length)]
-    lengths = list(map(max, zip(*[map(len, c) for c in commits])))
 
+    # Calculate column length
+    columns = list(map(max, zip(*[map(len, c) for c in commits])))
+
+    # Print commit history
     for commit in commits:
         output = ""
-        components = []
 
-        for i, (value, length) in enumerate(zip(commit, lengths)):
+        for i, (value, length) in enumerate(zip(commit, columns)):
             if i == 2:
+                # Format date
                 year, month, day = value.split("-")
                 output += day + "." + month + "." + year
                 output += " " * (length + 1 - len(value))
             elif i == 4:
+                # Format changes
                 files, *parts = value.split(", ")
                 output += files
+
                 if parts:
                     tup = []
                     for part in parts:
@@ -52,5 +62,6 @@ try:
                 output += separator
 
         print(output.strip())
+
 except subprocess.CalledProcessError as e:
     print("An error occurred: " + e)
