a9fe80fe54ff49e82a47e1fc51850fdb4a19051c
omeyenburg <oskar.meyenburg@gmail.com>
2024-09-29 16:22:44 +0200

    Nvim: improve performance in large files

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -13,6 +13,14 @@ https://github.com/nvim-treesitter/nvim-treesitter-context
 
 --]]
 
+local function disable(_, buf)
+    local max_size = 10 -- Kilobytes
+    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+    if (not ok) or stats and stats.size > 1024 * max_size then
+        return true
+    end
+end
+
 return {
     { -- Syntax parser
         'nvim-treesitter/nvim-treesitter',
@@ -24,21 +32,25 @@ return {
             auto_install = true,
             highlight = {
                 enable = true,
-                -- If you are experiencing weird indenting issues, add the language to
-                -- the list of additional_vim_regex_highlighting and disabled languages for indent.
                 additional_vim_regex_highlighting = { 'ruby' },
+                disable = disable,
             },
-            indent = { enable = true, disable = { 'ruby' } },
+            indent = {
+                enable = true,
+                disable = disable,
+            },
+            ts_context_commentstring = {
+                enable = true,
+                disable = disable,
+            },
+            incremental_selection = {
+                enable = true,
+                disable = disable,
+            },
         },
         config = function(_, opts)
             require('nvim-treesitter.install').prefer_git = true
             require('nvim-treesitter.configs').setup(opts)
         end,
-    },
-
-    { -- Current context
-        'nvim-treesitter/nvim-treesitter-context',
-        lazy = true,
-        event = { 'BufReadPost', 'BufNewFile' },
     },
 }
