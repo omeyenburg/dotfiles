95f2588f13a172370e525efacfb00c7ba9927d7e
omeyenburg <oskar.meyenburg@gmail.com>
2024-10-01 21:43:02 +0200

    Cheapino: swap double quote & underscore

--- .qmk_keyboards/cheapino/keymaps/colemak/keymap.c
+++ .qmk_keyboards/cheapino/keymaps/colemak/keymap.c
@@ -106,19 +106,16 @@ Qwerty layer
                      '--------------------'  '--------------------'
 
 */
+
 #include QMK_KEYBOARD_H
 #include "keymap_german_mac_iso.h"
 #include "quantum.h"
-
-#define K_ASTERISK KC_PAST
-#define K_MINUS KC_PAST + 1
-#define K_PLUS KC_PAST + 2
 
 enum custom_keycodes {
     K_LAYER1 = SAFE_RANGE,
     K_CYCLE_LANGUAGE,
     K_TILDE,      // ~
-    K_GRAVE,      // ``
+    K_GRAVE,      // `
     K_CIRCUMFLEX, // ^
     K_COLON,
     K_SEMICOLON,
@@ -260,7 +257,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
                 shift(DE_ACUT);
                 tap_code16(KC_SPC);
             } else {
-                tap_code16(DE_LABK);
+                tap_code16(KC_LABK);
+                tap_code16(KC_SPC);
             }
             return false;
         case K_CIRCUMFLEX: // "^" symbol
@@ -287,7 +285,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
             return false;
         case K_AT:
             if (system_keyboard_layout == 0) {
-                /*altgr(KC_L);*/
                 tap_code16(DE_AT);
             } else {
                 tap_code16(KC_AT);
@@ -468,52 +465,52 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 
 // Define the keymap using LAYOUT_split_3x5_3
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_3x5_3(                                                                                                                                                // Colemak
-        KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, K_COLON,                                                                                                       //
-        KC_A, KC_R, KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I, KC_O,                                                                                                          //
-        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_K, KC_M, MT(MOD_LGUI, KC_DOT), MT(MOD_RALT, KC_COMM), K_SEMICOLON,                                                                  //
-        TD(TD_SHIFT_CAPS), KC_BSPC, TD(TD_CTRL_ESC), TD(TD_ENTER_TAB), KC_SPC, K_LAYER1                                                                                      //
-    ),                                                                                                                                                                       //
-    [1] = LAYOUT_split_3x5_3(                                                                                                                                                // Numbers
-        K_AT, K_BACKSLASH, K_PIPE, K_SLASH, K_LEFT_CURLY_BRACKET, K_RIGHT_CURLY_BRACKET, KC_1, KC_2, KC_3, KC_DOT,                                                           //
-        MT(MOD_RALT, K_ASTERISK), MT(MOD_LGUI, K_PLUS), MT(MOD_LCTL, K_MINUS), K_DOUBLE_QUOTE, K_LEFT_BRACKET, K_RIGHT_BRACKET, KC_4, KC_5, KC_6, KC_0,                         //
-        K_HASH, K_GRAVE, K_UNDERSCORE, K_QUOTE, K_LEFT_SQUARE_BRACKET, K_RIGHT_SQUARE_BRACKET, KC_7, KC_8, KC_9, KC_COMM,                                                    //
-        TO(0), KC_BSPC, KC_DEL, TD(TD_ENTER_TAB), KC_SPC, TO(2)                                                                                                              //
-    ),                                                                                                                                                                       //
-    [2] = LAYOUT_split_3x5_3(                                                                                                                                                // Symbols
-        K_AT, K_BACKSLASH, K_PIPE, K_SLASH, K_EQUAL, K_PERCENT, K_CIRCUMFLEX, K_DOLLAR, K_TILDE, K_DIACRITIC,                                                                //
-        MT(MOD_RALT, K_ASTERISK), MT(MOD_LGUI, K_PLUS), MT(MOD_LCTL, K_MINUS), K_DOUBLE_QUOTE, K_LEFT_ANGLE_BRACKET, K_RIGHT_ANGLE_BRACKET, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, //*
-        K_HASH, K_GRAVE, K_UNDERSCORE, K_QUOTE, K_EXCLAIM, K_QUESTION, K_AND, KC_DOT, KC_COMM, K_ESZETT,                                                                     //
-        TO(0), KC_BSPC, KC_DEL, TD(TD_ENTER_TAB), KC_SPC, TO(3)                                                                                                              //
-    ),                                                                                                                                                                       //
-    [3] = LAYOUT_split_3x5_3(                                                                                                                                                // Mouse
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,                                                                                        //
-        KC_HOME, KC_END, KC_PGUP, KC_PGDN, KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,                                                                                 //
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5,                                                                                      //
-        TO(0), KC_NO, KC_NO, KC_NO, KC_NO, TO(4)                                                                                                                             //
-    ),                                                                                                                                                                       //
-    [4] = LAYOUT_split_3x5_3(                                                                                                                                                // Functions and media
-        KC_F1, KC_F2, KC_F3, KC_F4, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                                                                //
-        KC_F5, KC_F6, KC_F7, KC_F8, KC_NO, KC_PSCR, KC_MPLY, KC_MSTP, KC_MNXT, KC_MPRV,                                                                                      //
-        KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_VOLU, KC_VOLD, KC_NO, KC_NO,                                                                                         //
-        TO(0), KC_NO, KC_NO, KC_NO, KC_NO, TO(5)                                                                                                                             //
-    ),                                                                                                                                                                       //
-    [5] = LAYOUT_split_3x5_3(                                                                                                                                                // Options
-        QK_RBT, KC_NO, KC_BRIU, GU_OFF, CG_SWAP, RGB_VAI, RGB_SAI, RGB_HUI, KC_NO, KC_NO,                                                                                    //
-        KC_NO, KC_NO, KC_BRID, GU_ON, CG_NORM, RGB_VAD, RGB_SAD, RGB_HUD, KC_NO, RGB_TOG,                                                                                    //
-        KC_NO, KC_NO, KC_WHOM, KC_WBAK, KC_WFWD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                                                          //
-        TO(0), TO(6), K_CYCLE_LANGUAGE, KC_NO, KC_NO, KC_NO                                                                                                                  //
-    ),                                                                                                                                                                       //
-    [6] = LAYOUT_split_3x5_3(                                                                                                                                                // Qwerty
-        KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,                                                                                                          //
-        KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, RALT(KC_U),                                                                                                    //
-        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, MT(MOD_LGUI, KC_DOT), MT(MOD_RALT, KC_COMM), DE_SS,                                                                        //
-        TD(TD_SHIFT_CAPS), KC_BSPC, TD(TD_CTRL_ESC), TD(TD_ENTER_TAB), KC_SPC, TO(7)                                                                                         //
-    ),                                                                                                                                                                       //
-    [7] = LAYOUT_split_3x5_3(                                                                                                                                                // Numbers
-        RALT(KC_L), RSA(KC_7), RALT(KC_7), DE_SLSH, RALT(KC_8), RALT(KC_9), KC_1, KC_2, KC_3, DE_DOT,                                                                        //
-        MT(MOD_RALT, KC_PAST), MT(MOD_LGUI, DE_PLUS), MT(MOD_LCTL, DE_MINS), DE_DQUO, DE_LPRN, DE_RPRN, KC_4, KC_5, KC_6, KC_0,                                              //
-        DE_HASH, K_GRAVE, DE_UNDS, DE_QUOT, RALT(KC_5), RALT(KC_6), KC_7, KC_8, KC_9, DE_COMM,                                                                               //
-        TO(6), KC_BSPC, KC_DEL, TD(TD_ENTER_TAB), KC_SPC, TO(0)                                                                                                              //
+    [0] = LAYOUT_split_3x5_3(                                                                                                                                                              // Colemak
+        KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, K_COLON,                                                                                                                     //
+        KC_A, KC_R, KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I, KC_O,                                                                                                                        //
+        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_K, KC_M, MT(MOD_LGUI, KC_DOT), MT(MOD_RALT, KC_COMM), K_SEMICOLON,                                                                                //
+        TD(TD_SHIFT_CAPS), KC_BSPC, TD(TD_CTRL_ESC), TD(TD_ENTER_TAB), KC_SPC, K_LAYER1                                                                                                    //
+    ),                                                                                                                                                                                     //
+    [1] = LAYOUT_split_3x5_3(                                                                                                                                                              // Numbers
+        K_AT, K_BACKSLASH, K_PIPE, K_SLASH, K_LEFT_CURLY_BRACKET, K_RIGHT_CURLY_BRACKET, KC_1, KC_2, KC_3, KC_DOT,                                                                         //
+        MT(MOD_RALT, KC_KP_ASTERISK), MT(MOD_LGUI, KC_KP_PLUS), MT(MOD_LCTL, KC_KP_MINUS), K_UNDERSCORE, K_LEFT_BRACKET, K_RIGHT_BRACKET, KC_4, KC_5, KC_6, KC_0,                        //
+        K_HASH, K_GRAVE, K_QUOTE, K_DOUBLE_QUOTE, K_LEFT_SQUARE_BRACKET, K_RIGHT_SQUARE_BRACKET, KC_7, KC_8, KC_9, KC_COMM,                                                                  //
+        TO(0), KC_BSPC, KC_DEL, TD(TD_ENTER_TAB), KC_SPC, TO(2)                                                                                                                            //
+    ),                                                                                                                                                                                     //
+    [2] = LAYOUT_split_3x5_3(                                                                                                                                                              // Symbols
+        K_AT, K_BACKSLASH, K_PIPE, K_SLASH, K_EQUAL, K_PERCENT, K_CIRCUMFLEX, K_DOLLAR, K_TILDE, K_DIACRITIC,                                                                              //
+        MT(MOD_RALT, KC_KP_ASTERISK), MT(MOD_LGUI, KC_KP_PLUS), MT(MOD_LCTL, KC_KP_MINUS), K_UNDERSCORE, K_LEFT_ANGLE_BRACKET, K_RIGHT_ANGLE_BRACKET, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, //*
+        K_HASH, K_GRAVE, K_QUOTE, K_DOUBLE_QUOTE, K_EXCLAIM, K_QUESTION, K_AND, KC_DOT, KC_COMM, K_ESZETT,                                                                                   //
+        TO(0), KC_BSPC, KC_DEL, TD(TD_ENTER_TAB), KC_SPC, TO(3)                                                                                                                            //
+    ),                                                                                                                                                                                     //
+    [3] = LAYOUT_split_3x5_3(                                                                                                                                                              // Mouse
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,                                                                                                      //
+        KC_HOME, KC_END, KC_PGUP, KC_PGDN, KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,                                                                                               //
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5,                                                                                                    //
+        TO(0), KC_NO, KC_NO, KC_NO, KC_NO, TO(4)                                                                                                                                           //
+    ),                                                                                                                                                                                     //
+    [4] = LAYOUT_split_3x5_3(                                                                                                                                                              // Functions and media
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                                                                              //
+        KC_F5, KC_F6, KC_F7, KC_F8, KC_NO, KC_PSCR, KC_MPLY, KC_MSTP, KC_MNXT, KC_MPRV,                                                                                                    //
+        KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_VOLU, KC_VOLD, KC_NO, KC_NO,                                                                                                       //
+        TO(0), KC_NO, KC_NO, KC_NO, KC_NO, TO(5)                                                                                                                                           //
+    ),                                                                                                                                                                                     //
+    [5] = LAYOUT_split_3x5_3(                                                                                                                                                              // Options
+        QK_RBT, KC_NO, KC_BRIU, GU_OFF, CG_SWAP, RGB_VAI, RGB_SAI, RGB_HUI, KC_NO, KC_NO,                                                                                                  //
+        KC_NO, KC_NO, KC_BRID, GU_ON, CG_NORM, RGB_VAD, RGB_SAD, RGB_HUD, KC_NO, RGB_TOG,                                                                                                  //
+        KC_NO, KC_NO, KC_WHOM, KC_WBAK, KC_WFWD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                                                                        //
+        TO(0), TO(6), K_CYCLE_LANGUAGE, KC_NO, KC_NO, KC_NO                                                                                                                                //
+    ),                                                                                                                                                                                     //
+    [6] = LAYOUT_split_3x5_3(                                                                                                                                                              // Qwerty
+        KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,                                                                                                                        //
+        KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, RALT(KC_U),                                                                                                                  //
+        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, MT(MOD_LGUI, KC_DOT), MT(MOD_RALT, KC_COMM), DE_SS,                                                                                      //
+        TD(TD_SHIFT_CAPS), KC_BSPC, TD(TD_CTRL_ESC), TD(TD_ENTER_TAB), KC_SPC, TO(7)                                                                                                       //
+    ),                                                                                                                                                                                     //
+    [7] = LAYOUT_split_3x5_3(                                                                                                                                                              // Numbers
+        RALT(KC_L), RSA(KC_7), RALT(KC_7), DE_SLSH, RALT(KC_8), RALT(KC_9), KC_1, KC_2, KC_3, DE_DOT,                                                                                      //
+        MT(MOD_RALT, KC_PAST), MT(MOD_LGUI, DE_PLUS), MT(MOD_LCTL, DE_MINS), DE_DQUO, DE_LPRN, DE_RPRN, KC_4, KC_5, KC_6, KC_0,                                                            //
+        DE_HASH, K_GRAVE, DE_UNDS, DE_QUOT, RALT(KC_5), RALT(KC_6), KC_7, KC_8, KC_9, DE_COMM,                                                                                             //
+        TO(6), KC_BSPC, KC_DEL, TD(TD_ENTER_TAB), KC_SPC, TO(0)                                                                                                                            //
     ),
 };
