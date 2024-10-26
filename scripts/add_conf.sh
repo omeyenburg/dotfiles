ec0c53e83cf0a368d0867ca477c1c7554f54c8da
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2024-10-26 10:46:32 +0200

    Setup: create add_conf script

---
+++ scripts/add_conf.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+(
+    source scripts/err.sh
+
+    SCRIPT_NAME="add_conf.sh"
+
+    name=$1
+
+    if [ ! "$name" ]; then
+        err "No config name provided."
+    fi
+
+    CONFDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../config
+
+    mv -i "$HOME/.config/$name" "$CONFDIR" || err "Failed to move config file."
+
+    echo "Moved config."
+    echo "You should now reinstall it by running scripts/link_conf.sh"
+)
--- scripts/err.sh
+++ scripts/err.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+RED="\033[0;31m"
+NON="\033[0m"
+
 function err() {
     if [ $SCRIPT_NAME ]; then
-        echo -e "\033[0;31mError in module $SCRIPT_NAME: $@" 1>&2;
+        echo -e "${RED}Error in module $SCRIPT_NAME: $@${NON}" 1>&2;
     else
-        echo -e "\033[0;31mError: $@" 1>&2;
+        echo -e "${RED}Error: $@${NON}" 1>&2;
     fi
     exit 1;
 }
