8b304d9bbeb2e0d2a2019ec2debdb3af927e96cd
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2024-11-10 09:08:41 +0100

    Hyprland: media overlay & cleanup

--- config/hypr/scripts/media.sh
+++ config/hypr/scripts/media.sh
@@ -17,11 +17,11 @@ if [ "$1" = "volume" ]; then
     percent=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{ print 100 * $2 }')
 
     if [ "$muted" ]; then
-        title="Volume (muted)"
+        title="Volume muted"
     else
         title="Volume"
     fi
-elif [ "$1" = "screen-brightness" ]; then
+elif [ "$1" = "display-brightness" ]; then
     if [ "$2" = "up" ]; then
         brightnessctl set 5%+ > /dev/null
     elif [ "$2" = "down" ]; then
@@ -31,7 +31,7 @@ elif [ "$1" = "screen-brightness" ]; then
         exit 1
     fi
 
-    title="Screen Brightness"
+    title="Display Brightness"
     current=$(brightnessctl g)
     max=$(brightnessctl m)
     percent=$(echo "100*$current/$max" | bc)
@@ -50,26 +50,11 @@ elif [ "$1" = "keyboard-brightness" ]; then
     max=$(brightnessctl -d smc::kbd_backlight max)
     percent=$(echo "100*$current/$max" | bc)
 else
-    echo "Invalid value '$1'. Valid values are: volume, screen-brightness, keyboard-brightness"
+    echo "Invalid value '$1'. Valid values are: volume, display-brightness, keyboard-brightness"
     exit 1
 fi
 
-# killall -9 AppRun.wrapped
-# killall -9 zenity
-# $HOME/.local/bin/yad --progress --title="info-overlay" --text="$title" --text-align="center" --timeout=1 --no-buttons --borders=12 $percent
-# old=$(ps -e | grep zenity | head -1 | awk '{ print $1 }')
-# zenity --progress --title="Volume" --text="" --timeout=1 --percentage=1 --no-cancel --ok-label="$percent%" --height=130 &
-# sleep 0.2
-# if [ "$old" ]; then
-#     echo "killing $old"
-#     kill $old
-# fi
-# pid=$(ps aux | grep "hypr/scripts/overlay.py" | awk '{ print $2 }')
-# sleep 0.2
-# kill -9 $pid
-
-echo $percent > /tmp/hypr-overlay-pipe
-running=$(ps aux | grep "python3.*hypr/scripts/overlay.py" | wc -l)
-if [ $running -le 1 ]; then
-    python3 $HOME/.config/hypr/scripts/overlay.py $percent
+echo $percent $title > /tmp/hyprland-media-overlay
+if [ $(ps aux | grep "python3.*hypr/scripts/overlay.py" | wc -l) -le 1 ]; then
+    python3 $HOME/.config/hypr/scripts/overlay.py
 fi
--- config/hypr/scripts/overlay.py
+++ config/hypr/scripts/overlay.py
@@ -1,62 +1,22 @@
-import time
-import sys
 import os
 import gi
-
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GLib
 
-try:
-    percentage = int(sys.argv[1])
-except ValueError or IndexError:
-    raise Exception("Expected one numeric argument: percentage")
 
-
-win = Gtk.Window()
-
-win.set_title("info-overlay")
-win.set_opacity(0.8)
-win.set_size_request(200, 200)
-win.connect("destroy", Gtk.main_quit)
-
-vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-vbox.set_homogeneous(True)
-win.add(vbox)
-
-# Add a label
-label = Gtk.Label(label="Volume")
-vbox.pack_start(label, False, False, 0)
-
-# Add an image
-image_path = os.path.join(os.getenv("HOME"), ".config/hypr/icons/audio-volume-high-symbolic.png")
-image = Gtk.Image.new_from_file(image_path)
-vbox.pack_start(image, False, False, 0)
-
-# Add a progress bar
-progress_bar = Gtk.ProgressBar()
-progress_bar.set_fraction(percentage / 100)
-progress_bar.set_margin_start(30)
-progress_bar.set_margin_end(30)
-vbox.pack_start(progress_bar, False, False, 0)
-
-# Function to close the window after 1 second
-def close_window():
-    win.close()
-    return False  # Returning False removes the timeout callback after it runs once
-
-# Set a timeout to close the window after 1 second
-timeout_time = 800
-timeout_id = GLib.timeout_add(timeout_time, close_window)
-
-style_provider = Gtk.CssProvider()
 css = """
 label {
     color: #D3D3D3;
-    font-size: 25px;
+    font-size: 18px;
     font-weight: bold;
 }
 
+progressbar {
+    color: #D3D3D3;
+}
+
 progressbar > trough {
+    border: none;
     background-color: #808080;
     min-height: 10px;
 }
@@ -67,38 +27,114 @@ progressbar > trough > progress {
     min-height: 10px;
 }
 """
-style_provider.load_from_data(css.encode('utf-8'))
-Gtk.StyleContext.add_provider_for_screen(
-    Gtk.Window.get_screen(win),
-    style_provider,
-    Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
-)
+
+images = {
+    "empty": ".config/hypr/icons/empty.png",
+    "audio-low": ".config/hypr/icons/audio-volume-low-symbolic.png",
+    "audio-medium": ".config/hypr/icons/audio-volume-medium-symbolic.png",
+    "audio-high": ".config/hypr/icons/audio-volume-high-symbolic.png",
+    "audio-muted": ".config/hypr/icons/audio-volume-muted-symbolic.png",
+    "audio-overamplified": ".config/hypr/icons/audio-volume-overamplified-symbolic.png",
+    "display-brightness": ".config/hypr/icons/display-brightness-symbolic.png",
+    "keyboard-brightness": ".config/hypr/icons/keyboard-brightness-symbolic.png"
+}
 
 
-win.show_all()
+class Window(Gtk.Window):
+    def __init__(self):
+        super().__init__()
+        self.widgets()
+        self.connect("destroy", Gtk.main_quit)
+        self.set_title("media-overlay")
+        self.set_opacity(0.8)
+        self.set_size_request(200, 200)
 
-FIFO_PATH = "/tmp/hypr-overlay-pipe"  # Named pipe path
-# Function to read from the FIFO and update the progress bar
+        # Apply custom css
+        style_provider = Gtk.CssProvider()
+        style_provider.load_from_data(css.encode('utf-8'))
+        Gtk.StyleContext.add_provider_for_screen(
+            Gtk.Window.get_screen(self),
+            style_provider,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        )
 
+        timeout_id = GLib.timeout_add(1000, self.quit)
+        GLib.idle_add(self.update, timeout_id)
+        self.show_all()
+        Gtk.main()
 
-def update_progress():
-    global timeout_id
+    def widgets(self):
+        # Widget container
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
+        vbox.set_homogeneous(True)
+        self.add(vbox)
 
-    with open(FIFO_PATH, "r+") as fifo:
-        line = fifo.read().strip()
-        if line:
-            try:
-                percent = float(line) / 100
-                progress_bar.set_fraction(percent)
+        # Title label
+        self.title_widget = Gtk.Label(label="")
+        vbox.pack_start(self.title_widget, False, False, 0)
+
+        # Media icon
+        icon_path = os.path.join(os.getenv("HOME"), images["empty"])
+        self.icon_widget = Gtk.Image.new_from_file(icon_path)
+        vbox.pack_start(self.icon_widget, False, False, 0)
+
+        # Bar
+        self.bar_widget = Gtk.ProgressBar()
+        self.bar_widget.set_margin_start(23)
+        self.bar_widget.set_margin_end(23)
+        self.bar_widget.set_show_text(True)
+        vbox.pack_start(self.bar_widget, False, False, 0)
+
+    def update(self, timeout_id):
+        with open("/tmp/hyprland-media-overlay", "r+") as pipe:
+            line = pipe.read().strip().split()
+            if len(line) >= 2:
+                try:
+                    percent = int(line[0])
+                except ValueError:
+                    print("Could not convert to int:", line[0])
+
+                if "muted" in line:
+                    title = " ".join(line[1:-2])
+                    muted = True
+                else:
+                    title = " ".join(line[1:])
+                    muted = False
+
+                self.title_widget.set_text(title)
+                self.bar_widget.set_fraction(percent / 100)
+                self.bar_widget.set_text(str(percent) + "%")
+
+                if title == "Volume":
+                    if muted:
+                        icon = images["audio-muted"]
+                    elif percent < 33:
+                        icon = images["audio-low"]
+                    elif percent < 66:
+                        icon = images["audio-medium"]
+                    elif percent < 100:
+                        icon = images["audio-high"]
+                    else:
+                        icon = images["audio-overamplified"]
+                elif title == "Display Brightness":
+                    icon = images["display-brightness"]
+                else:
+                    icon = images["keyboard-brightness"]
+
+                icon_path = os.path.join(os.getenv("HOME"), icon)
+                self.icon_widget.set_from_file(icon_path)
+
                 GLib.source_remove(timeout_id)
-                timeout_id = GLib.timeout_add(timeout_time, close_window)
-                fifo.truncate(0)
-            except ValueError:
-                print("Invalid input:", line)
-    GLib.timeout_add(50, update_progress)
+                timeout_id = GLib.timeout_add(800, self.quit)
+                pipe.truncate(0)
+
+        GLib.timeout_add(50, self.update, timeout_id)
+        return False
+
+    def quit(self):
+        self.close()
+        return False
 
 
-# Run the update_progress function in a separate thread
-GLib.idle_add(update_progress)
-
-Gtk.main()
+if __name__ == "__main__":
+    Window()
