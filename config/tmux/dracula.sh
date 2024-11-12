ef0b9308fb8d40fe66631b48016633f0a213ce4b
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2024-11-12 19:00:31 +0100

    Tmux: delete temporary file after running sed on dracula

--- config/tmux/dracula.sh
+++ config/tmux/dracula.sh
@@ -5,5 +5,5 @@ temp_file="$(mktemp)"
 sed_script="$HOME/.config/tmux/dracula.sed"
 dracula_script="$HOME/.tmux/plugins/tmux/scripts/dracula.sh"
 
-sed -f $sed_script $dracula_script > $temp_file
-cat $temp_file > $dracula_script
+sed -f $sed_script $dracula_script > $temp_file || (echo "Dracula sed script failed! Aborting." && exit 1)
+mv $temp_file $dracula_script
