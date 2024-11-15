e33c4c09299b0546eee3ff24a926ed9b15bb92d7
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2024-11-15 21:08:53 +0100

    Hyprland: use hyprlock instead of swaylock

--- config/hypr/scripts/logout.sh
+++ config/hypr/scripts/logout.sh
@@ -16,7 +16,7 @@ style=$HOME/.config/hypr/wofi/logout/style.css
 output=$(echo -e "$s0\n$s1\n$s2\n$s3\n$s4" | wofi --show dmenu --conf $conf --style $style)
 
 case "$output" in
-    "$s0") swaylock --config $HOME/.config/hypr/swaylock/config.toml ;;
+    "$s0") loginctl lock-session ;;
     "$s1") hyprctl dispatch exit ;;
     "$s2") systemctl reboot ;;
     "$s3") systemctl poweroff ;;
