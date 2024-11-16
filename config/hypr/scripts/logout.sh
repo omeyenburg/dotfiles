585c26f6e3c68d7751c8ca22b9b7e1ccbb6e90dd
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2024-11-16 22:11:42 +0100

    Hyprland: wal colors for wofi menus

--- config/hypr/scripts/logout.sh
+++ config/hypr/scripts/logout.sh
@@ -9,17 +9,14 @@ s0="          Lock"
 s1="        Logout"
 s2="        Reboot"
 s3="      Shutdown"
-s4="         Gnome"
 
 conf=$HOME/.config/hypr/wofi/logout/config.toml
 style=$HOME/.config/hypr/wofi/logout/style.css
-output=$(echo -e "$s0\n$s1\n$s2\n$s3\n$s4" | wofi --show dmenu --conf $conf --style $style)
+output=$(echo -e "$s0\n$s1\n$s2\n$s3" | wofi --show dmenu --conf $conf --style $style)
 
 case "$output" in
     "$s0") loginctl lock-session ;;
     "$s1") hyprctl dispatch exit ;;
     "$s2") systemctl reboot ;;
     "$s3") systemctl poweroff ;;
-    "$s4") XDG_CURRENT_DESKTOP=GNOME gnome-control-center --verbose ;;
-    *) ;;
 esac
