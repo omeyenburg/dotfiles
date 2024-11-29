f1505e18a196a98280cc25ea2a3c48b3ab5afd9f
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2024-11-29 06:39:26 +0100

    Make nvim default git editor

--- scripts/git.sh
+++ scripts/git.sh
@@ -3,7 +3,7 @@
 # Set up git configuration
 git config --global user.name "omeyemburg"
 git config --global user.email "135001586+omeyenburg@users.noreply.github.com"
-git config --global core.editor "vim"
+git config --global core.editor "nvim"
 git config --global core.excludesfile "~/.config/git/gitignore_global"
 git config --global fetch.autoFetch true
 git config --global color.ui true
