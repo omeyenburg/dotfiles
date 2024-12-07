a2b4432a2753d115515eec144afddcf9e5ac66eb
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2024-12-07 16:32:43 +0100

    NixOS: updated config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -4,28 +4,46 @@
 # | |\  | |>  <| |_| |___) |
 # |_| \_|_/_/\_\\___/|____/
 #
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
 { config, lib, pkgs, ... }:
 
 {
-  # System {{{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
-  imports = [
-    ./hardware-configuration.nix # Include the results of the hardware scan.
-  ];
-  
+  # Use the systemd-boot EFI boot loader.
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
+
   boot = {
+    # Clean temporary files in /tmp on every boot.
     tmp.cleanOnBoot = true;
+
+    # Use latest stable Linux kernel available in NixOS.
     kernelPackages = pkgs.linuxPackages_latest;
-    kernelModules = [ "btusb" ];  # If your chip is supported by this module
-    kernelParams = [ 
-      "bluetooth.disable_ertm=1"  # Disable Enhanced Retransmission Mode
-      "i915.enable_psr=0"         # Disables Panel Self Refresh to prevent flickering/stuttering.
-      "i915.enable_fbc=1"         # Enables Framebuffer Compression for better performance.
-      "i915.enable_guc=3"         # Enables GuC for better GPU scheduling (Broadwell supports this).
-      "apm=power_off"
-      "acpi=force"
-     ];
+
+    # Load btusb module, commonly used for Bluetooth adapters.
+    # Only used if the chip is supported by this module.
+    kernelModules = [ "btusb" ];
+
+    # kernelParams = [ 
+      # Recommendation: Yes, if you experience Bluetooth issues like stuttering. Otherwise, it’s not essential. 
+      # "bluetooth.disable_ertm=1"  # Disable Enhanced Retransmission Mode
+
+      # Recommendation: Yes, especially if you notice flickering or stuttering in video playback or UI rendering.
+      # "i915.enable_psr=0"         # Disables Panel Self Refresh to prevent flickering/stuttering.
+
+      # Recommendation: Yes, as it benefits performance without significant downsides.
+      # "i915.enable_fbc=1"         # Enables Framebuffer Compression for better performance.
+
+      # Recommendation: Yes, as it can improve performance, especially with newer kernels.
+      # "i915.enable_guc=3"         # Enables GuC for better GPU scheduling (Broadwell supports this).
+     # ];
 
     # Use the systemd-boot EFI boot loader.
     loader = {
@@ -34,28 +52,37 @@
     };
   };
 
-  # }}}
-  # Networking & Bluetooth {{{  
+  networking = {
+    # Define hostname.
+    hostName = "oskar-nixos";
 
-  # Define your hostname.
-  networking.hostName = "oskar-nixos";
+    # Pick only one of the below networking options.
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Networking options (use only one)
-  # networking.wireless.enable = true;      # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+    # Configure network proxy if necessary.
+    # proxy.default = "http://user:password@proxy:port/";
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  
+    # Open ports in the firewall.
+    # firewall.allowedTCPPorts = [ ... ];
+    # firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # firewall.enable = false;
+  };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8"; # default?
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  console = {
+    # font = "Lat2-Terminus16";
+    # keyMap = "us";
+    earlySetup = true;
+    useXkbConfig = true;
+  };
 
   # Enable bluetooth
   hardware.bluetooth = {
@@ -73,132 +100,169 @@
     };
   };
 
-  services.blueman.enable = true;
+  services = {
+    # Enable blueman interface
+    blueman.enable = true;
 
-  # }}}
-  # Localization & Internationalization {{{  
+    # Configure keymap in X11
+    xserver.xkb = {
+      layout = "de";
+      variant = "mac";
+      options = "eurosign:e,caps:escape";
+    };
 
-  # Set your time zone
-  time.timeZone = "Europe/Berlin";
+    # Enable the X11 windowing system.
+    # xserver.enable = true;
+  
+    # Configure keymap in X11.
+    # xserver.xkb.layout = "us";
+    # xserver.xkb.options = "eurosign:e,caps:escape";
+  
+    # Enable CUPS to print documents.
+    printing.enable = true;
+  
+    # Enable sound (pulseaudio or pipewire).
+    # hardware.pulseaudio.enable = true;
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+    };
+  
+    # Enable touchpad support (enabled default in most desktopManager).
+    # libinput.enable = true;
+  
+    # Enable power profiles daemon.
+    power-profiles-daemon.enable = true;
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  console = {
-    earlySetup = true;
-    useXkbConfig = true;
+    # Enable the OpenSSH daemon.
+    # openssh.enable = true;
   };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "de";
-    variant = "mac";
-    options = "eurosign:e,caps:escape";
+  # Define a user account.
+  # Don't forget to set a password with ‘passwd’.
+  users.users.oskar = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      yazi
+      tmux
+      kitty
+      emacs
+      neovim
+      ripgrep
+      fastfetch
+      unzip
+      gcc
+      gnumake
+      python313
+      gimp
+      spotify
+      discord
+      obsidian
+      inkscape
+      mars-mips
+      logisim
+      logisim-evolution
+      hunspell
+      hunspellDicts.de_DE
+      hunspellDicts.en_US-large
+      libreoffice-qt
+      zathura
+      texliveBasic
+      yad
+      wofi
+      mako
+      pywal
+      waybar
+      figlet
+      hyprland
+      hypridle
+      hyprlock
+      hyprshot
+      hyprpaper
+      hyprpicker
+    ];
   };
 
-  # }}}
-  # Graphics & Display {{{
+  # Allow non-free software (e.g. spotify).
+  nixpkgs.config.allowUnfree = true;
 
-  # Graphics
-  services.xserver.videoDrivers = [ "intel" ];
-  hardware.graphics.enable = true;
-  hardware.graphics.extraPackages = [
-    pkgs.intel-vaapi-driver 
-    # pkgs.intel-ocl # good for opencl
-    # pkgs.intel-compute-runtime # good for opencl
-    # pkgs.intel-media-driver # probably not needed or used; for newer cpu
-
-    # VA-API
-    pkgs.libva
-    pkgs.libvdpau
-    pkgs.vaapiIntel
-    # pkgs.vaapiVdpau # Optional, unless you need VDPAU-to-VAAPI translation (for older or specific apps).
-
-    pkgs.ffmpeg
-    pkgs.ustreamer
-  ];
-
-  services.udisks2.enable = true;
-  services.upower.enable = true;
-
-  # Enable sound (pipewire or pulseaudio)
-  # hardware.pulseaudio = {
-  #   enable = true;
-  #   package = pkgs.pulseaudioFull;
-  # };
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    pulse.enable = true;
-  };
-
-  programs.firefox.enable = true;
-  programs.hyprland.enable = true;
-
-  # Enable CUPS to print documents
-  services.printing.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Enable power profiles daemon
-  services.power-profiles-daemon.enable = true;
-
-  # }}}
-  # Environment Variables {{{
-  environment.sessionVariables = rec {
-    XDG_CACHE_HOME  = "$HOME/.cache";
-    XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_DATA_HOME   = "$HOME/.local/share";
-    XDG_STATE_HOME  = "$HOME/.local/state";
-    XDG_BIN_HOME    = "$HOME/.local/bin";
-    XDG_CURRENT_DESKTOP = "Hyprland";
-
-    PATH = [ 
-      "${XDG_BIN_HOME}"
+  environment = {
+    # Packages installed in system profile.
+    systemPackages = with pkgs; [
+      git
+      vim
+      btop
+      tree
+      wget
+      curl
+      acpi
+      xorg.xrdb
+      wl-clipboard
+      brightnessctl
+      power-profiles-daemon
+      adwaita-qt
+      adwaita-qt6
+      intel-gpu-tools
+      bluez
+      bluez-tools
+      linux-firmware
+      b43FirmwareCutter
+      b43Firmware_6_30_163_46
+      broadcom-bt-firmware
     ];
 
-    # Cursor                                                                           
-    # XCURSOR_SIZE = "24";
-    # XCURSOR_THEME = "Adwaita";
- 
-    # Toolkit backend
-    # GTK_THEME = "Adwaita-dark";
-    # GDK_BACKEND = "wayland,x11,*";
-    # CLUTTER_BACKEND = "wayland";
-
-    # QT config
-    # QT_QPA_PLATFORM = "wayland;xcb";
-    # QT_QPA_PLATFORMTHEME = "qt5ct";
-    # QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    # QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-
-    # Electron config
-    # ELECTRON_ENABLE_SYSTEM_THEME = "true";
-    # ELECTRON_FORCE_SYSTEM_THEME = "1";
-
-    # Firefox
-    # MOZ_ENABLE_WAYLAND = "1";
-    # MOZ_WEBRENDER = "1";
-    # MOZ_ACCELERATED = "1";
-    # MOZ_DISABLE_RDD_SANDBOX = "1";
-
-    # General config
-    # EDITOR = "nvim";
-    # TERMINAL = "kitty";
-    # FONT = "FiraCode Nerd Font";
+    # Define environment variables for session processes.
+    sessionVariables = rec {
+      XDG_CACHE_HOME  = "$HOME/.cache";
+      XDG_CONFIG_HOME = "$HOME/.config";
+      XDG_DATA_HOME   = "$HOME/.local/share";
+      XDG_STATE_HOME  = "$HOME/.local/state";
+      XDG_BIN_HOME    = "$HOME/.local/bin";
+      XDG_CURRENT_DESKTOP = "Hyprland";
+      PATH = [ "${XDG_BIN_HOME}" ];
   
-    # Video acceleration
-    LIBVA_DRIVER_NAME = "i965";  # or "iHD" for newer Intel GPUs
-    VDPAU_DRIVER = "va_gl";
-
-    # Remove if running into compatibility issues
-    # SDL_VIDEODRIVER = "wayland";
-
-    # Configure nmtui colors
-    # Elements: root, border, window, shadow, title, button, actbutton, checkbox, actcheckbox, entry, label, listbox, actlistbox, textbox, acttextbox, helpline, roottext, emptyscale, fullscale, disentry, compactbutton, actsellistbox, sellistbox
-    # Syntax: <element>=<fg>,<bg>
-    NEWT_COLORS = ''
+      # Cursor                                                                           
+      # XCURSOR_SIZE = "24";
+      # XCURSOR_THEME = "Adwaita";
+   
+      # Toolkit backend
+      # GTK_THEME = "Adwaita-dark";
+      # GDK_BACKEND = "wayland,x11,*";
+      # CLUTTER_BACKEND = "wayland";
+  
+      # QT config
+      # QT_QPA_PLATFORM = "wayland;xcb";
+      # QT_QPA_PLATFORMTHEME = "qt5ct";
+      # QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+      # QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+  
+      # Electron config
+      # ELECTRON_ENABLE_SYSTEM_THEME = "true";
+      # ELECTRON_FORCE_SYSTEM_THEME = "1";
+  
+      # Firefox
+      # MOZ_ENABLE_WAYLAND = "1";
+      # MOZ_WEBRENDER = "1";
+      # MOZ_ACCELERATED = "1";
+      # MOZ_DISABLE_RDD_SANDBOX = "1";
+  
+      # General config
+      # EDITOR = "nvim";
+      # TERMINAL = "kitty";
+      # FONT = "FiraCode Nerd Font";
+    
+      # Video acceleration
+      # LIBVA_DRIVER_NAME = "i965";  # or "iHD" for newer Intel GPUs
+      # VDPAU_DRIVER = "va_gl";
+  
+      # Remove if running into compatibility issues
+      # SDL_VIDEODRIVER = "wayland";
+  
+      # Configure nmtui colors
+      # Elements: root, border, window, shadow, title, button, actbutton, checkbox, actcheckbox, entry, label, listbox, actlistbox, textbox, acttextbox, helpline, roottext, emptyscale, fullscale, disentry, compactbutton, actsellistbox, sellistbox
+      # Syntax: <element>=<fg>,<bg>
+      NEWT_COLORS = ''
         root=black,black
         border=black,lightgray
         window=white,lightgray
@@ -219,106 +283,13 @@
         disentry=black,blue
         actsellistbox=black,blue
         sellistbox=black,blue
-    '';
+      '';
+    };
   };
 
-  # }}}
-  # Users {{{
-
-  # Define a user account. Don't forget to set a password with passwd.
-  users.users.oskar = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable sudo for the user
-    packages = with pkgs; [
-      pkgs.yazi
-      pkgs.tmux
-      pkgs.kitty
-      pkgs.emacs
-      pkgs.neovim
-      pkgs.ripgrep
-      pkgs.fastfetch
-
-      unzip
-      libgcc
-      gnumake
-      python313
-
-      pkgs.gimp
-      pkgs.spotify
-      pkgs.discord
-      pkgs.obsidian
-      pkgs.inkscape
-      pkgs.mars-mips
-      pkgs.logisim
-      pkgs.logisim-evolution
-
-      pkgs.hunspell
-      pkgs.hunspellDicts.de_DE
-      pkgs.hunspellDicts.en_US-large
-      pkgs.libreoffice-qt
-
-      pkgs.zathura
-      pkgs.texliveBasic
-
-      pkgs.yad
-      pkgs.wofi
-      pkgs.mako
-      pkgs.pywal
-      pkgs.waybar
-      pkgs.figlet
-
-      pkgs.hyprland
-      pkgs.hypridle
-      pkgs.hyprlock
-      pkgs.hyprshot
-      pkgs.hyprpaper
-      pkgs.hyprpicker
-    ];
-  };
-
-  # }}}
-  # Packages {{{
-
-  # List packages installed in system profile
-  environment.systemPackages = with pkgs; [
-    pkgs.git
-    pkgs.vim
-    pkgs.btop
-    pkgs.tree
-    pkgs.wget
-    pkgs.curl
-    pkgs.libgcc
-
-    pkgs.acpi
-    pkgs.xorg.xrdb
-    pkgs.wl-clipboard
-    pkgs.brightnessctl
-    pkgs.power-profiles-daemon
-
-    pkgs.adwaita-qt
-    pkgs.adwaita-qt6
-    # pkgs.xsettingsd
-    # pkgs.libsForQt5.qt5ct
-
-    pkgs.intel-gpu-tools
-
-    # Bluetooth
-    pkgs.bluez
-    pkgs.bluez-tools
-    pkgs.linux-firmware
-    pkgs.b43FirmwareCutter
-    pkgs.b43Firmware_6_30_163_46
-    pkgs.broadcom-bt-firmware
-  ];
-
-  # Allow non-free software (e.g. spotify)
-  nixpkgs.config.allowUnfree = true;
-
-  # }}}
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
+  # Some programs need SUID wrappers, can be configured further or are started in user sessions.
+  programs.firefox.enable = true;
+  programs.hyprland.enable = true;
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
