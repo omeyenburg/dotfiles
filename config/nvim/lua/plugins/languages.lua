51af61339848e0b72068d0d2d91305c81b759547
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2024-12-28 23:38:28 +0100

    Nvim: remove render-markdown plugin

--- config/nvim/lua/plugins/languages.lua
+++ config/nvim/lua/plugins/languages.lua
@@ -4,10 +4,6 @@
 Latex compilation & live preview
 https://github.com/lervag/vimtex
 
-# Render Markdown
-Render pretty markdown
-https://github.com/MeanderingProgrammer/render-markdown.nvim
-
 # Rust Tools
 https://github.com/simrat39/rust-tools.nvim
 
@@ -27,30 +23,13 @@ return {
     },
 
     {
-        'MeanderingProgrammer/render-markdown.nvim',
-        lazy = true,
-        ft = { 'markdown' },
-        opts = {},
-    },
-
-    {
         'simrat39/rust-tools.nvim',
         lazy = true,
         ft = { 'rust' },
         opts = {
-            tools = {
-                autoSetHints = true,
-                inlay_hints = {
-                    show_parameter_hints = true,
-                    parameter_hints_prefix = '<- ',
-                    other_hints_prefix = '=> ',
-                    max_len_align = false,
-                    right_align = false,
-                },
-            },
             server = {
+                standalone = true,
                 on_attach = function(_, bufnr)
-                    -- Mappings for LSP
                     local opts = { noremap = true, silent = true }
                     local buf_set_keymap = vim.api.nvim_buf_set_keymap
                     buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
@@ -64,28 +43,11 @@ return {
                     buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
                     buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
                     buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-
-                    -- Depricated
-                    -- buf_set_keymap(bufnr, 'n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-                    -- buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-                    -- buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-                    -- buf_set_keymap(bufnr, 'n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-                    -- buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-
-                    local move_item = require('rust-tools').move_item.move_item
-                    buf_set_keymap(bufnr, 'v', 'K', function()
-                        move_item(true)
-                    end, { noremap = true, silent = true })
-                    buf_set_keymap(bufnr, 'v', 'J', function()
-                        move_item(false)
-                    end, { noremap = false, silent = true })
                 end,
                 settings = {
                     ['rust-analyzer'] = {
                         diagnostics = {
                             enable = true,
-                            disabled = {},
-                            enableExperimental = true,
                         },
                         cargo = {
                             allFeatures = true,
@@ -105,6 +67,6 @@ return {
     {
         'Saecki/crates.nvim',
         lazy = true,
-        ft = { 'rust' },
+        ft = { 'rust', 'toml' },
     },
 }
