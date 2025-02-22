373f172e4b7d56ee8f76b3bca98c8f5352548748
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2025-02-22 22:59:23 +0100

    nvim: allow lsp inlay hints

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -194,6 +194,9 @@ return {
                 config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
                 lspconfig[server].setup(config)
             end
+
+            -- Enable inlay hints, e.g. implicit types
+            vim.lsp.inlay_hint.enable(true)
         end,
     },
 }
