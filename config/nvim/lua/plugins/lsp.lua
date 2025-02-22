7ca89300afb17455dc5afb4d851ce01a722457dc
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2025-02-22 22:58:46 +0100

    nvim: add rust analyzer config back to lsp config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -13,8 +13,8 @@ return {
         priority = 800,
         event = { 'BufReadPre', 'BufNewFile' },
         dependencies = {
-            { 'j-hui/fidget.nvim', opts = {} }, -- Useful status updates for LSP
-            { 'saghen/blink.cmp' }, -- Completion
+            'j-hui/fidget.nvim',
+            'saghen/blink.cmp',
         },
         opts = {
             inlay_hints = { enabled = true },
@@ -22,7 +22,7 @@ return {
                 -- haskell-language-server
                 hls = {},
 
-                -- nixd (e.g., for Nix files)
+                -- nixd (for Nix files)
                 nixd = {},
 
                 -- clang-tools (Clangd for C/C++/Objective-C/Objective-C++)
@@ -35,13 +35,6 @@ return {
                 lua_ls = {
                     settings = {
                         Lua = {
-                            runtime = {
-                                version = 'LuaJIT', -- Lua runtime version
-                                path = vim.split(package.path, ';'),
-                            },
-                            diagnostics = {
-                                globals = { 'vim' }, -- Recognize 'vim' as a global
-                            },
                             workspace = {
                                 library = vim.api.nvim_get_runtime_file('', true), -- Make LSP aware of Neovim runtime files
                                 checkThirdParty = false,
@@ -59,10 +52,48 @@ return {
                 -- jedi-language-server (Python)
                 jedi_language_server = {},
 
-                -- latex language server
+                -- latex-language-server
                 texlab = {},
 
-                -- mips language server
+                -- rust-analyzer
+                rust_analyzer = {
+                    cachePriming = {
+                        enable = false,
+                    },
+                    check = {
+                        command = 'clippy',
+                    },
+                    checkOnSave = true,
+                    completion = {
+                        fullFunctionSignatures = {
+                            enable = true,
+                        },
+                    },
+                    diagnostics = {
+                        disabled = { 'unlinked-file' },
+                    },
+                    inlayHints = {
+                        closureCaptureHints = {
+                            enable = true,
+                        },
+                        closureReturnTypeHints = {
+                            enable = 'always',
+                        },
+                        implicitDrops = {
+                            enable = true,
+                        },
+                        genericParameterHints = {
+                            lifetime = {
+                                enable = true,
+                            },
+                            type = {
+                                enable = true,
+                            },
+                        },
+                    },
+                },
+
+                -- mips-language-server
                 mips_ls = {
                     settings = {
                         Mips = {
@@ -87,13 +118,10 @@ return {
                         vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
                     end
 
-                    -- Jump to the definition of the word under your cursor.
+                    map('K', vim.lsp.buf.hover, 'Hover Documentation')
+                    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
                     map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-
-                    -- Find references for the word under your cursor.
                     map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-                    -- Jump to the implementation of the word under your cursor.
                     map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
                     -- Jump to the type of the word under your cursor.
@@ -112,48 +140,6 @@ return {
                     -- or a suggestion from your LSP for this to activate.
                     map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-                    -- Opens a popup that displays documentation about the word under your cursor
-                    map('K', vim.lsp.buf.hover, 'Hover Documentation')
-
-                    -- WARN: This is not Goto Definition, this is Goto Declaration.
-                    --  For example, in C this would take you to the header.
-                    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-                    -- NOTE: The following two autocommands are used to highlight references of the
-                    -- word under your cursor when your cursor rests there for a little while.
-                    -- When you move your cursor, the highlights will be cleared (the second autocommand).
-                    local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-                    -- if client and client.server_capabilities.documentHighlightProvider then
-                    --     local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
-                    --     vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-                    --         buffer = event.buf,
-                    --         group = highlight_augroup,
-                    --         callback = vim.lsp.buf.document_highlight,
-                    --     })
-                    --
-                    --     vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-                    --         buffer = event.buf,
-                    --         group = highlight_augroup,
-                    --         callback = vim.lsp.buf.clear_references,
-                    --     })
-                    --
-                    --     vim.api.nvim_create_autocmd('LspDetach', {
-                    --         group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
-                    --         callback = function(event2)
-                    --             vim.lsp.buf.clear_references()
-                    --             vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
-                    --         end,
-                    --     })
-                    -- end
-
-                    -- Toggle inlay hints
-                    if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-                        map('<leader>th', function()
-                            vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled())
-                        end, '[T]oggle Inlay [H]ints')
-                    end
-
                     -- Autocompletion selection highlights
                     vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { bg = 'NONE', strikethrough = true, fg = '#808080' })
                     vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { bg = 'NONE', fg = '#569CD6' })
@@ -166,6 +152,26 @@ return {
                     vim.api.nvim_set_hl(0, 'CmpItemKindKeyword', { bg = 'NONE', fg = '#D4D4D4' })
                     vim.api.nvim_set_hl(0, 'CmpItemKindProperty', { link = 'CmpItemKindKeyword' })
                     vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { link = 'CmpItemKindKeyword' })
+
+                    -- Should prevent rust-analyzer from crashing with code -32802
+                    for _, method in ipairs { 'textDocument/diagnostic', 'workspace/diagnostic' } do
+                        local default_diagnostic_handler = vim.lsp.handlers[method]
+                        vim.lsp.handlers[method] = function(err, result, context, config)
+                            if err and err.code == -32802 then
+                                -- Check if the result contains a progress value
+                                local progress = result and result.params and result.params.value
+
+                                -- If the progress kind is "end", then let it pass through
+                                if progress and progress.kind == 'end' then
+                                    return default_diagnostic_handler(err, result, context, config)
+                                end
+
+                                -- Otherwise, ignore the error
+                                return
+                            end
+                            return default_diagnostic_handler(err, result, context, config)
+                        end
+                    end
                 end,
             })
 
