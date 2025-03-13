771c680bf584995a72146a320353d29b9c562923
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2025-03-13 19:48:37 +0100

    update nixos backup script

--- scripts/nixos-backup.sh
+++ scripts/nixos-backup.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-NIXOSDIR="$DOTFILES/nixos"
-
-cat /etc/nixos/home.nix > "$NIXOSDIR/home.nix"
-cat /etc/nixos/flake.nix > "$NIXOSDIR/flake.nix"
-cat /etc/nixos/configuration.nix > "$NIXOSDIR/configuration.nix"
-cat /etc/nixos/macos-configuration.nix > "$NIXOSDIR/macos-configuration.nix"
+cp -r /etc/nixos/* "$DOTFILES"/nixos
