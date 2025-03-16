996285ae4e52ab903df6aa871d1a21ccd7ab9a72
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2025-03-16 22:00:25 +0100

    feat(tmux): linkie.nvim session script

---
+++ config/tmux/repos/linkie-nvim.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+session=linkie-nvim
+repo=~/git/linkie.nvim/
+
+# Check if the session already exists
+tmux has-session -t $session 2>/dev/null
+
+# $? is the exit status of the last command
+if [ $? != 0 ]; then
+    tmux new-session -d -s $session -c "$repo"
+
+    tmux new-window -t $session:1
+    tmux new-window -t $session:2 -c "$repo"
+    tmux new-window -t $session:3 -c "$repo"
+
+    tmux select-window -t $session:1
+fi
+
+# Attach to the tmux session
+tmux attach -t $session
