fb7678f111523bc1fc9c250041dffa0230b123be
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2025-03-16 21:58:58 +0100

    feat(setup): automatically clone catppuccin repo for tmux

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+
 source scripts/err.sh
+
+if [ ! -f ~/.config/tmux/tmux.conf ]; then
+    err "Please run \`make link\` first."
+fi
 
 SCRIPT_NAME="tmux.sh"
 PLUGINS=$HOME/.tmux/plugins
@@ -8,10 +13,15 @@ PLUGINS=$HOME/.tmux/plugins
     echo Installing tpm
     mkdir -p "$PLUGINS" && cd "$PLUGINS" || err "Failed to create $PLUGINS"
     cd tpm && git pull ||
-        git clone git@github.com:tmux-plugins/tpm $HOME/.tmux/plugins/tpm ||
+        git clone git@github.com:tmux-plugins/tpm "$HOME"/.tmux/plugins/tpm ||
         err "Could not clone tpm"
 
     cd "$PLUGINS"
     echo Installing plugins
     source "$PLUGINS"/tpm/scripts/install_plugins.sh || err "Failed to install plugins"
 )
+
+# Check whether catppuccin is actively used in config
+if grep "run ~/.tmux/plugins/catppuccin/catppuccin.tmux" config/tmux/tmux.conf > /dev/null; then
+    git clone git@github.com:catppuccin/tmux.git ~/.tmux/plugins/catppuccin
+fi
