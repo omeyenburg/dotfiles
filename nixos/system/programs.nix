a3d21dd4df2c756a26ab08dece5908ca7393fc3d
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2025-03-24 08:44:18 +0100

    feat(nixos): add thunderbird and gnupg programs

--- nixos/system/programs.nix
+++ nixos/system/programs.nix
@@ -1,8 +1,5 @@
-{
+{pkgs, ...}: {
   programs = {
-    # Install firefox.
-    firefox.enable = true;
-
     # Install gamemode wrapper for games.
     gamemode = {
       enable = true;
@@ -27,9 +24,23 @@
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     };
 
-    # gnupg.agent = {
-    #   enable = true;
-    #   enableSSHSupport = true;
-    # };
+    gnupg.agent = {
+      enable = true;
+      # enableSSHSupport = true; # Optional: Allows using GPG keys as SSH keys
+      # enableExtraSocket = true; # Optional: Useful for tools like pass or browser integration
+      # enableBrowserSocket = false; # Set to true if using GPG in the browser (e.g., for web-based password managers)
+      pinentryPackage = pkgs.pinentry-curses; # Non-GUI-prompt
+      settings = {
+        # Passphrase stays cached for 20 minutes after use up to 4 hours
+        default-cache-ttl = 1200;
+        max-cache-ttl = 7200;
+      };
+    };
+
+    thunderbird = {
+      enable = true;
+      preferences = {};
+      preferencesStatus = "default";
+    };
   };
 }
