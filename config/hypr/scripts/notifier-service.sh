4c722b00b1f3c017014ebdf41ce5a4118993cbcd
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2025-03-24 08:40:27 +0100

    fix(emails): only locally mark emails as seen by storing id in cache

--- config/hypr/scripts/notifier-service.sh
+++ config/hypr/scripts/notifier-service.sh
@@ -20,12 +20,19 @@ fi
 scheme=imaps
 port=993
 
+mailcache=~/.cache/seen_mails
+mkdir -p ~/.cache
+
 sed 's/ /\n/g' ~/.netrc | sed '/machine/{n;p}' -n | while read -r server; do
     url="$scheme://$server:$port/INBOX"
     mails=$(curl --url "$url" --connect-timeout 1 --netrc --request "SEARCH UNSEEN" -s | sed 's/^* SEARCH\s*//;s/\s*$//')
 
     for mail in $mails; do
-        subject_request="FETCH $mail (BODY[HEADER.FIELDS (SUBJECT)])"
+        # Skip if already seen
+        grep -q "^$mail$" "$mailcache" && continue
+        echo "$mail" >>$mailcache
+
+        subject_request="FETCH $mail (BODY.PEEK[HEADER.FIELDS (SUBJECT)])"
         subject=$(curl --url "$url" --connect-timeout 1 --netrc --request "$subject_request" -D - -s | sed -n '/^Subject:/{s/Subject:\s*//;s/\s*$//;p}')
 
         echo "New email: $subject"
