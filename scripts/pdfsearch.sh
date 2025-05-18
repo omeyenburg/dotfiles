10ea82f0931f9d4cff70f71a94f3b6f0fc629c48
omeyemburg <oskar.git@gmx.net>
2025-05-18 16:44:20 +0200

    feat(pdfsearch): search for pdf names or for pdf content using ripgrep

--- scripts/pdfsearch.sh
+++ scripts/pdfsearch.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
 
 tmpfile=/tmp/pdfsearch
-pdfs=$(find ~/books/ ~/Downloads/ ~/git/university/ -name "*.pdf" | sed 's/\/[^\/]*\/[^\/]*\///')
-kitty --class pdfsearch sh -c "echo \"$pdfs\" | fzf --preview 'pdftotext -f 1 -l 2 -raw -nodiag -nopgbrk -raw {} - | head -10' > $tmpfile"
-cat $tmpfile | xargs -r zathura
+directories="$HOME/books/ $HOME/Downloads/ $HOME/git/university/"
+
+# Search mode: "name" or "content"
+mode=${1:-"name"}
+
+if [ "$mode" = "name" ]; then
+    pdfs=$(find $directories -name "*.pdf" | sed 's/\/[^\/]*\/[^\/]*\///')
+    kitty --class pdfsearch sh -c "echo \"$pdfs\" | fzf --preview 'pdftotext -f 1 -l 2 -raw -nodiag -nopgbrk -raw {} - | head -10' > $tmpfile"
+    file=$(cat $tmpfile)
+
+    if [ -n "$file" ]; then
+        zathura "$file" &
+        disown
+    fi
+elif [ "$mode" = "content" ]; then
+    kitty --class pdfsearch sh -c "true | fzf --prompt='🔍 Search PDFs: ' --bind \"change:reload:if [ -n {q} ]; then rga --type pdf --no-heading --color=always {q} $directories; else true; fi\" --ansi > $tmpfile"
+    file_with_page=$(cat $tmpfile | sed 's/\(.*\.pdf\):Page \([0-9][0-9]*\): .*/\1 \2/')
+    file="${file_with_page% *}"
+    page="${file_with_page##* }"
+
+    if [ -n "$file" ] && [ -n "$page" ]; then
+        zathura --page="$page" "$file" &
+        disown
+    fi
+else
+    echo "Valid modes are: name, content."
+fi
