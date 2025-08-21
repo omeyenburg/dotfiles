b6c92f7d2515513c9c5446c6c14e841c7db1399e
omeyemburg <oskar.git@gmx.net>
2025-08-21 22:16:53 +0200

    chore(nvim): update treesitter mips

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -14,11 +14,11 @@ return {
     {
         'nvim-treesitter/nvim-treesitter',
         lazy = false,
-        branch = 'main',
+        branch = 'master',
         build = ':TSUpdate',
         opts = {
-            ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'rust', 'python' },
-            ignore_install = { 'latex', 'asm' },
+            ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'rust', 'python', 'mips' },
+            ignore_install = { 'latex' },
             auto_install = true,
             highlight = {
                 enable = true,
@@ -35,47 +35,44 @@ return {
             },
         },
         config = function(_, opts)
-            require('nvim-treesitter').setup(opts)
-
             -- Enable folding
             vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
             vim.wo.foldmethod = 'expr'
             vim.wo.foldlevel = 100
 
-            vim.keymap.set("n", "z0", ":set foldlevel=100<cr>")
-            vim.keymap.set("n", "z1", ":set foldlevel=0<cr>")
-            vim.keymap.set("n", "z2", ":set foldlevel=1<cr>")
-            vim.keymap.set("n", "z3", ":set foldlevel=2<cr>")
-            vim.keymap.set("n", "z4", ":set foldlevel=3<cr>")
-            vim.keymap.set("n", "z5", ":set foldlevel=4<cr>")
-            vim.keymap.set("n", "z6", ":set foldlevel=5<cr>")
-            vim.keymap.set("n", "z7", ":set foldlevel=6<cr>")
-            vim.keymap.set("n", "z8", ":set foldlevel=7<cr>")
-            vim.keymap.set("n", "z9", ":set foldlevel=8<cr>")
+            vim.keymap.set('n', 'z0', ':set foldlevel=100<cr>')
+            vim.keymap.set('n', 'z1', ':set foldlevel=0<cr>')
+            vim.keymap.set('n', 'z2', ':set foldlevel=1<cr>')
+            vim.keymap.set('n', 'z3', ':set foldlevel=2<cr>')
+            vim.keymap.set('n', 'z4', ':set foldlevel=3<cr>')
+            vim.keymap.set('n', 'z5', ':set foldlevel=4<cr>')
+            vim.keymap.set('n', 'z6', ':set foldlevel=5<cr>')
+            vim.keymap.set('n', 'z7', ':set foldlevel=6<cr>')
+            vim.keymap.set('n', 'z8', ':set foldlevel=7<cr>')
+            vim.keymap.set('n', 'z9', ':set foldlevel=8<cr>')
 
             -- Enable highlighting
             vim.api.nvim_create_autocmd('BufReadPost', {
                 callback = function()
-                    local buf = vim.api.nvim_get_current_buf()
-                    local lang = vim.treesitter.language.get_lang(vim.bo[buf].filetype)
-                    if lang and not vim.treesitter.highlighter.active[buf] then
-                        vim.treesitter.start(buf, lang)
-                    end
+                    pcall(vim.treesitter.start)
                 end,
             })
 
-            -- local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-            -- parser_config.mips = {
-            --     install_info = {
-            --         url = 'https://github.com/omeyenburg/tree-sitter-mips', -- You can use a local path for url if you prefer
-            --         -- url = vim.fn.expand '$HOME/git/tree-sitter-mips',
-            --         branch = 'main',
-            --         files = { 'src/parser.c' },
-            --         generate_requires_npm = false,
-            --         requires_generate_from_grammar = false,
-            --     },
-            --     filetype = 'asm',
-            -- }
+            -- Only possible on master branch (old version)
+            local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+            parser_config.mips = {
+                install_info = {
+                    -- url = 'https://github.com/omeyenburg/tree-sitter-mips',
+                    url = vim.fn.expand '$HOME/git/tree-sitter-mips',
+                    branch = 'main',
+                    files = { 'src/parser.c', 'src/scanner.c' },
+                    generate_requires_npm = false,
+                    requires_generate_from_grammar = false,
+                },
+                filetype = { 'asm', 'vmasm' },
+            }
+
+            require('nvim-treesitter').setup(opts)
         end,
     },
 
