5f1ee0b0acb255c855bc8642c7dbed3e7be0e6af
omeyemburg <oskar.git@gmx.net>
2025-09-17 21:11:16 +0200

    fix(nvim): swap keybind directions for cmd line completion

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -105,11 +105,11 @@ return {
             cmdline = {
                 keymap = {
                     preset = 'none',
-                    ['<Tab>'] = { 'select_prev' },
-                    ['<Up>'] = { 'select_prev' },
+                    ['<Tab>'] = { 'select_next' },
+                    ['<S-Tab>'] = { 'select_prev' },
+                    ['<Up>'] = { 'select_prev', 'fallback' },
+                    ['<Down>'] = { 'select_next', 'fallback' },
                     ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
-                    ['<S-Tab>'] = { 'select_next' },
-                    ['<Down>'] = { 'select_next' },
                     ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
                     ['<C-y>'] = { 'select_and_accept' },
                 },
