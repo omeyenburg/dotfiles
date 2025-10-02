5a679bd8d944ffb38633e78e5d36a9c561ed74fe
omeyenburg <omeyenburg@gmail.com>
2025-10-02 20:56:57 +0200

    chore(git): update public credentials

--- scripts/setup/git.sh
+++ scripts/setup/git.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Set up git configuration
-git config --global user.name "omeyemburg"
-git config --global user.email "oskar.git@gmx.net"
+git config --global user.name "omeyenburg"
+git config --global user.email "omeyenburg@gmail.com"
 git config --global core.editor "nvim"
 git config --global core.excludesfile "~/.config/git/gitignore_global"
 git config --global fetch.autoFetch true
